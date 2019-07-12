d0166bfa488b0527fc09ff23bc0732ff299a653e
Ziliang Luo <kennyluo4@gmail.com>
2019-07-12 14:03:54 -0700

    counting the transcript number in GTF/GFF file

---
+++ counting_transcripts.py
@@ -0,0 +1,18 @@
+def help():
+    print("usage: python counting_transcripts.py GTF file"
+          "counting the transcripts number in a GTF file")
+import sys
+argv = sys.argv
+f = argv[1]
+print(f)
+# f = open("stringtie_merged_pe_IDmodified.gtf")
+num = 0
+for lines in open(f):
+    if lines.startswith("#"):
+        continue
+    else:
+        trans = lines.split('\t')[2]
+        if trans == "transcript":
+            num += 1
+            print(lines.strip())
+print("Total transcript number is %d" % num)
