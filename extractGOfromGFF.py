761983299cdc442d11ee9aa587852136ac695ff5
ziliangluo <kennyluo4@gmail.com>
2019-08-05 15:08:29 -0400

    add summary number of genes

--- extractGOfromGFF.py
+++ extractGOfromGFF.py
@@ -6,17 +6,24 @@ import sys, csv
 argvs = sys.argv
 res = []
 file = argvs[1]
-
+# file = "arahy.Tifrunner.gnm1.ann1.CCJH.gene_models_main.gff3"
+num = 0
+go_num = 0
 for lines in open(file):
     if lines.startswith("#"):
         continue
-    elif "marker\tgene\t" in lines:
-        anno = lines.split("\t")[8]
+
+    anno, identifier = lines.split("\t")[8], lines.split("\t")[2]
+    if identifier == "gene":
+        num += 1
         go = [i.replace("Ontology_term=","") for i in anno.split(";") if i.startswith("Ontology_term")]
-        gene_name = anno.split(";")[1]
+        if len(go) != 0:
+            go_num += 1
+        gene_name = anno.split(";")[1].replace("Name=", "")
         go.insert(0, gene_name)
         res.append(go)
 
+print("gene number:%d, %d genes have GO id" % (num, go_num))
 with open("ref_gene_GO.csv", "w") as file:
     writer = csv.writer(file)
     writer.writerow(["gene name", "GOs"])
