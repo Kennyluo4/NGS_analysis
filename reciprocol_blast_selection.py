5b65dcd62d522ae9a82a0058aa386e603ac2da11
ziliangluo <kennyluo4@gmail.com>
2019-08-29 11:31:44 -0400

    update syntax

--- reciprocol_blast_selection.py
+++ reciprocol_blast_selection.py
@@ -2,31 +2,31 @@ Usage = """python reciprocol_blast_selection.py BLASTout1 BLASTout2 OutputFile "
 
 import sys, re
 def get_argv():
-	if len(sys.argv) != 4:
-		print(Usage)
-	else:
-		in1 = sys.argv[1]
-		in2 = sys.argv[2]
-		out = sys.argv[3]
-	return in1, in2, out
+    if len(sys.argv) != 4:
+        print(Usage)
+    else:
+        in1 = sys.argv[1]
+        in2 = sys.argv[2]
+        out = sys.argv[3]
+    return in1, in2, out
 
 def read_file(filename):
-	Dic = {}
-	for lines in open(filename):
-		if lines.startswith("#"):
-			continue
-		else:
-			itms = lines.strip().split('\t')
-			query, subject = itms[0], itms[1]
-			if query not in Dic.keys():
-				Dic[query] = subject
-	return	Dic
+    Dic = {}
+    for lines in open(filename):
+        if lines.startswith("#"):
+            continue
+        else:
+            itms = lines.strip().split('\t')
+            query, subject = itms[0], itms[1]
+            if query not in Dic.keys():
+                Dic[query] = subject
+    return Dic
 
 
 try:
-	infile1, infile2, outfile = get_argv()
+    infile1, infile2, outfile = get_argv()
 except UnboundLocalError:
-	exit()
+    exit()
 
 # infile1, infile2, outfile = "ahy_to_nod","nod_to_ahy","test"
 
@@ -36,13 +36,13 @@ D2 = read_file(infile2)
 
 SharedPairs = {}
 for k1, v1 in D1.items():
-	if v1 in D2.keys() and D2[v1] == k1:
-		SharedPairs[k1] = v1
+    if v1 in D2.keys() and D2[v1] == k1:
+        SharedPairs[k1] = v1
 
 with open(outfile, 'w') as file:
-	for k1 in SharedPairs.keys():
-		line = k1 + '\t' + SharedPairs[k1] + '\n'
-		file.write(line)
+    for k1 in SharedPairs.keys():
+        line = k1 + '\t' + SharedPairs[k1] + '\n'
+        file.write(line)
 file.close()
 print("Done. RBH from", sys.argv[1], "and", sys.argv[2], "are in", sys.argv[3])
 
