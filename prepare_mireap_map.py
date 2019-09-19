5db6b724fa97502fdc306dc3226396a8e6a04fbb
ziliangluo <kennyluo4@gmail.com>
2019-09-19 17:39:48 -0400

    get the reads mapping information from mirdeep2 .arf files and write to a map file needed for mireap

---
+++ prepare_mireap_map.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import sys
+
+f = sys.argv[1]
+res =[]
+newf = f.replace("_to_genome.arf", "_map.txt")
+print("start working on %s" % f)
+print("output file is %s" % newf)
+
+with open(f,'r') as handle:
+    with open(newf, 'w') as outhandle:
+        for lines in handle:
+            itms = lines.strip().split('\t')
+            read, chr, start, end, strand = itms[0].split("_x")[0] + "_x", itms[5], itms[7], itms[8], itms[10]
+            outhandle.write(read + "\t" + chr + "\t" + start + "\t" + end + "\t" + strand + "\n")
