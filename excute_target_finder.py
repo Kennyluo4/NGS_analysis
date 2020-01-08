d4cea2fee301695fcc1a318daaa7f62fbfb67c40
ziliangluo <kennyluo4@gmail.com>
2020-01-07 21:18:33 -0500

    excute the targetfinder.pl script for fasta file

---
+++ excute_target_finder.py
@@ -0,0 +1,25 @@
+import sys, os
+
+def help():
+    print("Usage:\npython excute_target_finder.py <miRNA.fasta> <path/targetfinder script> <transcriptfile>")
+
+try:
+    argvs = sys.argv
+    seqfile = argvs[1]
+    targetfinder = argvs[2]
+    transcriptfile = argvs[3]
+    print("miRNA sequence file is %s" % seqfile)
+    print("the targetfinder script with full path: %s" % targetfinder)
+except IndexError:
+    help()
+
+    # print("excute:\nperl %s -s %s -d transcripts.adj.fa "% (targetfinder, seq))
+for lines in open(seqfile):
+    if lines.startswith(">"):
+        mirnaID = lines.strip().replace(">", "")
+        continue
+    elif lines.startswith("#"):
+        continue
+    else:
+        seq = lines.strip()
+    os.system("perl %s -s %s -d transcripts.adj.fa -p table -q %s -t 4" % (targetfinder, seq, mirnaID))
