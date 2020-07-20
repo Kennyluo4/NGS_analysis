ddfce30cf6e1ba2a696668392177e13770d1e48d
ziliangluo <kennyluo4@gmail.com>
2020-07-19 22:19:25 -0400

    add others scripts

---
+++ others_useful_script/mstrg_gtf_file_prep.py
@@ -0,0 +1,43 @@
+#!/bin/env python3
+#Usage: python mstrg_prep.py stringtie_merged_se.gtf > stringtie_merged_se_IDmodified.gtf
+#appending refgene name to stringtie assigned ID (MSTRG.*) if the gene region includes a ref gene
+
+import re, fileinput
+g = {}     #gene_id => {ref_gene_ids}
+prep = []  #array of [line, mstrg_id]
+for line in fileinput.input():
+# for line in open("test.gtf"):
+    line = line.rstrip()
+    t = line.split('\t')
+    if len(t) < 9:
+        print(line)
+        continue
+    mgid = re.search('gene_id "(MSTRG\.\d+)"', t[8])     #search if GeneID in annotation column .
+    t_class = re.search('class_code "."', t[8])     # search the code for gff compare
+    if mgid:
+        gid = mgid.group(1)            #if matched geneID, assign to gid
+        prep.append([line, gid])        #append original line and extracted gene ID
+        mrn = re.search('gene_name "([^"]+)', t[8])
+        #g_name = re.search('gene_name "([^"]+)', t[8])     #find the gene_name
+        if t_class != None:
+            if ' "="' in t_class.group(0) or ' "j"' in t_class.group(0) or ' "e"' in t_class.group(0) or ' "s"' in t_class.group(0) or ' "o"' in t_class.group(0):
+                if mrn:
+                    gn = mrn.group(1)     #if find gene_name, assign to gn
+                    h = g.get(gid)
+                    if h:         #if gid in gene dic, append the gene name, if not add gene name to that gid
+                        h.add(gn)
+                    else:
+                      g[gid] = {gn}
+    else:
+        print(line)
+
+prevgid, gadd = '', ''
+for [line, gid] in prep:
+    if prevgid != gid:
+        gadd = ''
+        h = g.get(gid)
+        if h:
+            gadd = '|'+'|'.join(sorted(g[gid]))
+    if len(gadd) > 0:
+        line = re.sub('gene_id "MSTRG\.\d+', 'gene_id "'+gid+gadd, line)
+    print(line)
