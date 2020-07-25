7e13a12b97237083af30d43a6a66fc35be897919
ziliangluo <kennyluo4@gmail.com>
2020-07-25 18:57:19 -0400

    add total reads number and overall alignment rate

--- get_align_stat_star.py
+++ get_align_stat_star.py
@@ -12,12 +12,15 @@ def readStats(f):
     print('reading file ... %s' % f)
     sampleID = f.replace('Log.final.out','')
     row = [sampleID]
+    total = 0
     uniq = 0
     multi = 0
     multi_toomany = 0
     unmapped = 0
     for lines in open(f):
-        if 'Uniquely mapped reads number' in lines:
+        if 'Number of input reads |' in lines:
+            total = int(lines.split('|')[1].strip())
+        elif 'Uniquely mapped reads number' in lines:
             uniq = int(lines.split('|')[1].strip())
         elif 'Number of reads mapped to multiple loci' in lines:
             multi = int(lines.split('|')[1].strip())
@@ -25,7 +28,7 @@ def readStats(f):
             multi_toomany = int(lines.split('|')[1].strip())
         elif 'Number of reads unmapped: too many mismatches' in lines or 'Number of reads unmapped: too short' in lines or 'Number of reads unmapped: other' in lines:
             unmapped += int(lines.split('|')[1].strip())
-    row.append(uniq, multi, multi_toomany, unmapped)
+    row = [sampleID, total, uniq, multi, multi_toomany, unmapped]
     return row
 
 def main():
@@ -34,8 +37,9 @@ def main():
     for f in files:
         alignment = readStats(f)
         data.append(alignment)
-    df = pd.DataFrame(data, columns = ['Sample ID', 'UniqueAlign', 'Multi-Align', 'TooManyAlign', 'Unmapped'])
-    df.to_excel('STAR_alignment_statistics.xls')
+    df = pd.DataFrame(data, columns = ['Sample ID', 'TotalReadsPair', 'UniqueAlign', 'Multi-Align', 'TooManyAlign', 'Unmapped'])
+    df['overall%'] = (df['UniqueAlign'] + df['Multi-Align'])/df['TotalReadsPair']
+    df.to_excel('STAR_alignment_statistics.xlsx',index=False)
 
 if __name__ == "__main__":
     main()
